e7dcb218b51eb56042b0a73a6377ee10b5e269e9
ItsWhisp <elmenrandomm400@gmail.com>
2024-03-31 23:59:23 -0600

    Avance #1 Rediseño

---
+++ assets/js/index.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function () {
+    feather.replace(); // Cargar Iconos
+
+    /* ------------------------------- Background ------------------------------- */
+    const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+    const colors = ["#001f3f", "#0074D9"];
+
+    const randBorderRadius = () =>
+        [...Array(4).keys()].map((x) => rand(30, 85) + "%").join(" ") +
+        " / " +
+        [...Array(4).keys()].map((x) => rand(30, 85) + "%").join(" ");
+
+    const genBlobs = () => {
+        [...Array(5).keys()].map((id) => {
+            const x = rand(25, 75);
+            const y = rand(25, 75);
+            const color = colors[rand(0, colors.length)];
+            createBlob({ x, y, color, id });
+        });
+    };
+
+    const createBlob = ({ id, x, y, color }) => {
+        const background = document.querySelector(".background");
+        const blob = document.createElement("div");
+
+        blob.id = `blob-${id}`;
+        blob.classList.add("blob");
+        blob.style.top = `${y}%`;
+        blob.style.left = `${x}%`;
+        blob.style.backgroundColor = color;
+        blob.style.scale = rand(1.25, 2);
+        blob.style.borderRadius = randBorderRadius();
+
+        background.appendChild(blob);
+        animateBlob(id);
+    };
+
+    const animateBlob = (id) => {
+        anime({
+            targets: `#blob-${id}`,
+            translateX: () => `+=${rand(-25, 25)}`,
+            translateY: () => `+=${rand(-25, 25)}`,
+            borderRadius: () => randBorderRadius(),
+            rotate: () => rand(-25, 25),
+            opacity: () => rand(0.4, 0.8),
+            scale: () => rand(1.25, 2),
+            duration: 5000,
+            easing: "easeInOutQuad",
+            complete: (anim) => animateBlob(id)
+        }).play();
+    };
+
+    genBlobs();
+
+    /* ----------------------------- Perspectiva 3D ----------------------------- */
+
+    let mouseX = 0;
+    let mouseY = 0;
+    const dropArea = document.getElementById('dropArea');
+
+    function updateTilt() {
+        const rect = dropArea.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const dx = mouseX - centerX;
+        const dy = mouseY - centerY;
+        const tiltX = dy * 0.05; // Sensibilidad en el eje Y
+        const tiltY = -dx * 0.01; // Sensibilidad en el eje X
+
+        dropArea.style.transform = `perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+
+        requestAnimationFrame(updateTilt);
+    }
+
+    function handleMouseMove(e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+    updateTilt();
+
+    /* ---------------------------- Menu de seleccion --------------------------- */
+
+    customSelect('select');
+});
--- index.php
+++ index.php
@@ -4,63 +4,69 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
 <!DOCTYPE html>
-<html>
+<html translate="no">
+<meta name="google" content="notranslate">
 
 <head>
-	<title>FILES (DEV)</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="assets/css/index.css">
-	<meta property="og:title" content="FILES (BETA)">
+    <title>FileBeam</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.svg">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <!-- <meta property="og:title" content="FILES (BETA)">
     <meta property="og:description" content="Comparte archivos rapidamente mediante enlaces directos">
     <meta property="og:url" content="files.jorge603.xyz">
-    <meta property="og:type" content="website">
+    <meta property="og:type" content="website"> -->
 </head>
 
 <body>
-	<img src="assets/favicon.ico">
-	<h1>Subir un archivo (DEV)</h1>
-	<?php
+    <?php
     $queryString = isset($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : '';
     ?>
-
-	<form class="fileForm" method="POST" action="<?php echo 'submit.php' . $queryString; ?>"
-		enctype="multipart/form-data">
-
-		<div id="dragDropArea">
-			<svg class="upload-icon secondary" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
-				<path
-					d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z">
-			</svg>
-			<svg class="file-icon secondary hidden" xmlns="http://www.w3.org/2000/svg" height="1em"
-				viewBox="0 0 384 512">
-				<path
-					d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
-			</svg>
-			<span class="secondary">Arrastre y suelte un archivo aquí o haga clic para seleccionar</span>
-			<span class="secondary"><strong>Tamaño maximo permitido: 100MB</strong></span>
-			<input type="file" id="fileInput" name="uploadedFile" />
-		</div>
-		<?php
-        if (isset($_SESSION['message']) && $_SESSION['message']) {
-            printf('<b id="output">%s</b>', $_SESSION['message']);
-            unset($_SESSION['message']);
-        }
-        ?>
-		<button disabled id="uploadBtn" type="submit" name="uploadBtn" value="Subir">
-			<span class="nf-icon">󰅧</span> Subir
-		</button>
-		<button class="hidden" id="gdpsBtn" name="gdpsBtn">
-			<span class="nf-icon">󰌹</span> Copiar y subir al GDPS
-		</button>
-	</form>
-	<div class="disclaimer">
-		<p class="secondary">Version de pruebas. Esto no representa el resultado final de FILES</p>
-		<p class="secondary">Al usar este sitio web, aceptas haber leido el <a href="disclaimer.html">disclaimer</a></p>
-		<p class="secondary">FILES (BETA) v0.2.0</p>
-	</div>
-	<script src="index.js"></script>
+    <div class="background">
+        <div class="blob"></div>
+    </div>
+    <div class="mainContainer">
+        <header>
+            <div class="logo">
+                <img class="appIcon" src="favicon.svg">
+                <span class="appName">FileBeam</span>
+                <span class="appVer">v0.2.0-dev</span>
+            </div>
+            <div class="links">
+                <a href="#">API</a>
+                <a href="#">FAQ</a>
+                <i data-feather="settings"></i>
+            </div>
+        </header>
+        <main>
+            <div id="dropArea">
+                <i data-feather="upload-cloud"></i>
+                <span>ARRASTRA Y SUELTA UN ARCHIVO AQUI</span>
+                <span class="divider">o</span>
+                <span class="sub">haz click para seleccionar</span>
+                <input type="file" id="fileInput" name="uploadedFile" />
+            </div>
+            <div class="expire">
+                <select name="expire" id="expireDropdown">
+                    <option value="expire-0">No Expirar</option>
+                    <option value="expire-2">Expirar en 2 horas</option>
+                    <option value="expire-6">Expirar en 6 horas</option>
+                    <option value="expire-12">Expirar en 12 horas</option>
+                    <option value="expire-24">Expirar en un dia</option>
+                </select>
+                <span>Tamaño Maximo: 100MB</span>
+            </div>
+            <button disabled id="uploadBtn" type="submit" name="uploadBtn" value="Subir" disabled>
+                <i data-feather="upload"></i> Subir
+            </button>
+        </main>
+    </div>
+    <script src="/assets/js/feather.min.js"></script>
+    <script src="/assets/js/custom-select.min.js"></script>
+    <script src="/assets/js/anime.min.js"></script>
+    <script src="/assets/js/index.js"></script>
 </body>
 
 </html>
