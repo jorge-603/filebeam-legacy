3bc39915a173e4c9cfbeb9288367ce87005261fe
Jorge603 <jorgean9999@gmail.com>
2024-04-07 22:29:06 -0600

    Cambio en el algoritmo de generacion de nombres aleatorios

--- lib/rename.php
+++ lib/rename.php
@@ -1,24 +1,12 @@
 <?php
 
-    //Inicializacion de la funcion para randomizar
-    function randomize($long) {
-
-        /**
-         * EXPLICACION DE LA FUNCION:
-         * Genera una cadena unica de caracteres aleatorios
-         * siguiendo parametros del servidor en el momento de
-         * generacion de nombres, ayudando a mantener un id unico para un archivo
-         * subido a files
-         **/
-
-
-
-        //TODO: hacer un mejor algoritmo de randomizacion :grin:
-
-        $randomstr = uniqid('', true); //Genera una serie de caracteres random unicos
-
-        $name = substr(str_replace('.', '', $randomstr), 0, $long); //Sanitiza el nombre
-
-        return $name; //retorna el nombre generado
+    # Mejorado del randomizado de los nombres aleatorios
+    function randomize($length) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        $maxIndex = strlen($characters) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $maxIndex)];
+        }
+        return $randomString;
     }
-?>
