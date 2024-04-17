198c3658916f2ef96c721a9d6a496b6a82d38ba1
Jorge603 <jorgean9999@gmail.com>
2024-04-16 22:47:08 -0600

    Nueva variable de configuracion

--- config/config.php
+++ config/config.php
@@ -11,3 +11,4 @@ $domain = 'localhost/files'; # Nombre del dominio o IP del servidor para la URL 
 $uploadFileDir = 'file' . '/'; # Directorio donde va a ser alojado el archivo
 $unallowedfileExtensions = array('js', 'jar', 'scr', 'cpl', 'jsp', 'doc', 'docx'); # Extensiones no permitidas.
 $maxFileSize = 100 * 1000 * 1000; # 100 * 1000 * 1000 = 100000000 Bytes = 100 MB 
+$maintenance = false; # Modo mantenimiento activado o desactivado
