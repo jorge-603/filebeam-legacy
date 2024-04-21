7aae47b11427e2a2c58cb667afabd95fed999cd9
Jorge603 <jorgean9999@gmail.com>
2024-04-20 23:27:19 -0600

    cambios menores

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@
  * VARIABLES DE CONFIGURACION.
  */
 
-$domain = 'localhost/files'; # Nombre del dominio o IP del servidor para la URL del archivo subido.
+$domain = 'localhost'; # Nombre del dominio o IP del servidor para la URL del archivo subido.
 $uploadFileDir = 'file' . '/'; # Directorio donde va a ser alojado el archivo
 $unallowedfileExtensions = array('js', 'jar', 'scr', 'cpl', 'jsp', 'doc', 'docx'); # Extensiones no permitidas.
 $maxFileSize = 100 * 1000 * 1000; # 100 * 1000 * 1000 = 100000000 Bytes = 100 MB 
