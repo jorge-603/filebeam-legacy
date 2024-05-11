7eca2ff3f025ebb7225f9bc360f1e8c86bb9a9d8
Jorge603 <jorgean9999@gmail.com>
2024-05-10 22:37:50 -0600

    Cambios menores

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once "sanitize.php";
+
 /**
  * ARCHIVO DE CONFIGURACION DEL PROYECTO
  * AQUI YACERÁN LAS VARIABLES DE CONFIGURACION DEL PROYECTO.
@@ -7,7 +9,8 @@
  * VARIABLES DE CONFIGURACION.
  */
 
-$domain = $_SERVER['SERVER_NAME']; # Dominio del servidor, esto es generado automaticamente.
+$uri = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; # URI generado automaticamente para el funcionamiento de la pagina.
+$uri = sanitize($uri);
 $uploadFileDir = '../' . 'file' . '/'; # Directorio donde va a ser alojado el archivo
 $unallowedfileExtensions = array('js', 'jar', 'scr', 'cpl', 'jsp', 'doc', 'docx'); # Extensiones no permitidas.
 $maxFileSize = 100 * 1000 * 1000; # 100 * 1000 * 1000 = 100000000 Bytes = 100 MB
